da57721d51647a139f9be518f8bdb0eb2a0f64ec
codyhh09 <codyhh09@gmail.com>
2018-07-03 13:46:14 -0400

    Override Ranking Grading System

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
@@ -9,5 +11,25 @@ namespace GradeBook.GradeBooks
             {
                 this.Type = GradeBookType.Ranked;
             }
+        
+        public override char GetLetterGrade(double averageGrade) {
+            if(this.Students.Count < 5)
+                throw new InvalidOperationException();
+            var threshold = (int)Math.Ceiling(Students.Count * .2);
+            var grades = this.Students
+                                .OrderByDescending(x => x.AverageGrade)
+                                .Select(x => x.AverageGrade)
+                                .ToList();
+            if(grades[threshold-1] <= averageGrade)
+                return 'A';
+            else if(grades[threshold*2-1] <= averageGrade)
+                return 'B';
+            else if(grades[threshold*3-1] <= averageGrade)
+                return 'C';
+            else if(grades[threshold*4-1] <= averageGrade)
+                return 'D';
+            else
+                return 'F';
+        } 
     }
 }
